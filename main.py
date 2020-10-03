46730a70bd5fed4d4aa64c59089277a0795edfbe
Suyash Chaudhary <54215216+suyashcsi@users.noreply.github.com>
2020-10-03 17:41:24 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import pyttsx3
 import PyPDF2
 
-book = open('IND.pdf','rb')
+book = open('sample.pdf','rb')
 pdfReader = PyPDF2.PdfFileReader(book)
 pages = pdfReader.numPages
 print(pages)
 speaker = pyttsx3.init()
-page = pdfReader.getPage(23)
+page = pdfReader.getPage(1)
 text = page.extractText()
 speaker.say(text)
 speaker.runAndWait()
